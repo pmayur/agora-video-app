a64bdc1eb87bb4d57e12c85d395c832ae28676d9
pmayur <thisispatil@gmail.com>
2020-12-27 00:58:23 +0530

    add: form component to get values

---
+++ src/components/ConfigForm/ConfigForm.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { withRouter } from 'react-router-dom'
+
+class ConfigForm extends React.Component {
+    constructor() {
+        super();
+
+        // state
+        this.state = {
+            channel : "",
+            token   : "",
+        };
+
+        // bindings
+        this.handleInputChange=this.handleInputChange.bind(this)
+        this.buttonClickHandler=this.buttonClickHandler.bind(this)
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value  = target.value;
+        const name   = target.name;
+
+        // update state using es6 compute property syntax
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    buttonClickHandler(event) {
+        const target = event.target;
+        const id     = target.id; // get the id name, host or audience
+
+        // change values in the App component
+        this.props.handleConfigChange({
+            channel: this.state.channel,
+            token: this.state.token,
+        })
+
+        this.props.history.push(`/${id}`) // route to id name
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    CHANNEL:
+                    <input
+                        name="channel"
+                        value={this.state.channel}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+
+                <label>
+                    TOKEN:
+                    <input
+                        name="token"
+                        value={this.state.token}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+
+                <button onClick={this.buttonClickHandler} id="host">
+                    JOIN AS A HOST
+                </button>
+
+                <button onClick={this.buttonClickHandler} id="audience">
+                    JOIN AS AUDIENCE
+                </button>
+            </div>
+        );
+    }
+}
+
+export default withRouter(ConfigForm);
