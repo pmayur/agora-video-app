577c6b40de4777461457f3af9a5ac51f001750e7
pmayur <thisispatil@gmail.com>
2020-12-27 00:58:48 +0530

    add: routes to the app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,65 @@
 import React from "react";
 
+import ConfigForm       from "./ConfigForm/ConfigForm";
+import TestComponent    from "./TestComponent" // placeholder component for routes
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 class App extends React.Component {
 
+    constructor() {
+        super()
+
+        // state
+        this.state = {
+            channel : process.env.REACT_APP_CHANNEL,
+            token   : process.env.REACT_APP_TOKEN
+        }
+
+        // bindings
+        this.handleConfigChange = this.handleConfigChange.bind(this);
+    }
+
+    handleConfigChange (object) {
+        const newToken   = object.token;
+        const newChannel = object.channel;
+
+        this.setState({
+            token   : newToken,
+            channel : newChannel
+        })
+    }
+
     render() {
-        return <div className="App">Hello World</div>;
+        return (
+            <Router>
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={() => (
+                            <ConfigForm
+                                handleConfigChange={this.handleConfigChange}
+                            />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/host"
+                        component={() => (
+                            <TestComponent channel={this.state.channel} /> //placeholder component for testing route
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/audience"
+                        component={() => (
+                            <TestComponent channel={this.state.channel} /> //placeholder component for testing route
+                        )}
+                    />
+                </Switch>
+            </Router>
+        );
     }
 }
 
