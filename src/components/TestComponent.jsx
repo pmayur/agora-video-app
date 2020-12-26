783f13aeffb0e6d572033b2b233ab15a58c9111e
pmayur <thisispatil@gmail.com>
2020-12-27 00:58:03 +0530

    add: a placeholder component for under dev routes

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// placeholder component for under development routes
+class TestComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.channel}
+            </div>
+        );
+    }
+}
+
+export default TestComponent;
