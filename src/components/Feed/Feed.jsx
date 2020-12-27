7180d3f11c6ec503a516eef9ac123d8fed27dc84
pmayur <thisispatil@gmail.com>
2020-12-27 19:13:22 +0530

    add: localfeed object to parent component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import AgoraRTC from "agora-rtc-sdk";
+
+import LocalFeed from "./LocalFeed"
+
+const CLIENT    = AgoraRTC.createClient({mode: "live", codec: "h264"})
 
 class Feed extends React.Component {
     constructor() {
         super();
 
         this.state = {
+            localStr: {},
             feedsList: {},
         };
+
+        // bindings
+        this.setLocalFeed = this.setLocalFeed.bind(this);
     }
+
+    setLocalFeed(localStr ,userId) {
+        let newLocalStr = {
+            [userId]: localStr
+        }
+
+        this.setState({
+            localStr: newLocalStr,
+        });
+    }
+
     render() {
         return (
             <>
-                <div id="local-feed"></div>
+                <LocalFeed
+                    client={CLIENT}
+                    setLocalFeed={this.setLocalFeed}
+                    isHost={this.props.isHost}
+                />
                 <div id="remote-feeds"></div>
                 <div id="feed-logs"></div>
             </>
