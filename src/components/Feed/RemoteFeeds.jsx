2458bc5481faa23d5461a5f0b0523612744535c9
pmayur <thisispatil@gmail.com>
2020-12-27 20:23:00 +0530

    add: remote feeds component
    - displays a list of new streams

---
+++ src/components/Feed/RemoteFeeds.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+class RemoteFeeds extends React.Component {
+
+    componentDidUpdate() {
+        let feedsIdList = Object.keys(this.props.feedsList)
+
+        feedsIdList.forEach( id => {
+            this.props.feedsList[id].play(`remote-feed-${id}`)
+        })
+    }
+
+    render() {
+
+        let remoteFeedsStyle = {
+            height: "400px",
+            width: "500px",
+            margin: '10px'
+        }
+
+        const feedsList = Object.values(this.props.feedsList);
+
+        return (
+            <div style={{display:'flex'}}>
+            {
+                feedsList.map(stream => {
+                    let streamId = stream.getId();
+                    return (
+                        <div
+                            key={streamId}
+                            id={`remote-feed-${streamId}`}
+                            style={remoteFeedsStyle}
+                        ></div>
+                    );
+                })
+            }
+            </div>
+        )
+    }
+}
+
+export default RemoteFeeds;
